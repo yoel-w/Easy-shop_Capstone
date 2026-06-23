2c79aa610fccecb9c896d582a953c2feb460c18b
Yoel W. <funinseattle@proton.me>
2026-06-23 14:17:19 -0700

    Remove filter stream to fix products not showing up and fix ability to edit products

--- src/main/java/org/yearup/service/ProductService.java
+++ src/main/java/org/yearup/service/ProductService.java
@@ -26,7 +26,6 @@ public class ProductService
                        .filter(p -> minPrice == null || p.getPrice() >= minPrice)
                        .filter(p -> maxPrice == null || p.getPrice() <= maxPrice)
                        .filter(p -> subCategory == null || subCategory.equalsIgnoreCase(p.getSubCategory()))
-                       .filter(Product::isFeatured)
                        .toList();
     }
 
@@ -56,6 +55,7 @@ public class ProductService
         existing.setSubCategory(product.getSubCategory());
         existing.setFeatured(product.isFeatured());
         existing.setImageUrl(product.getImageUrl());
+        existing.setStock(product.getStock());
         return productRepository.save(existing);
     }
 
