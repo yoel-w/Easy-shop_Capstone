f7d0bcbdcc4019650ba7214283739bcdf7bec3d9
Yoel W. <funinseattle@proton.me>
2026-06-24 14:52:01 -0700

    added methods for check out

--- src/main/java/org/yearup/service/CheckoutService.java
+++ src/main/java/org/yearup/service/CheckoutService.java
@@ -1,4 +1,20 @@
 package org.yearup.service;
 
+
+import org.springframework.stereotype.Service;
+import org.yearup.models.Checkout;
+import org.yearup.repository.CheckoutRepository;
+
+@Service
 public class CheckoutService {
+    private CheckoutRepository checkoutRepository;
+
+    public CheckoutService(CheckoutRepository checkoutRepository) {
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public Checkout getCheckout(long id) {
+
+    }
+
 }
